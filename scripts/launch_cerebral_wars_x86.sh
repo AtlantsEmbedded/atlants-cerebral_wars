6457ded98c9e667dd8a5807d3f7bcc608704c37e
Frederic Simard <atom2626@hotmail.com>
2016-05-04 13:34:34 -0400

    Update launch_cerebral_wars_x86.sh
    Fixing name of cerebral_wars application

--- scripts/launch_cerebral_wars_x86.sh
+++ scripts/launch_cerebral_wars_x86.sh
@@ -8,9 +8,9 @@ if [ $command == "start" ]; then
     atlants-DATA_interface/data-daemon/data_interface atlants-DATA_interface/data-daemon/config/data_config_player2.xml &
 	atlants-DATA_preprocessing/preprocess-daemon/data_preprocessing atlants-DATA_preprocessing/preprocess-daemon/config/preprocess_config_player1.xml &
 	atlants-DATA_preprocessing/preprocess-daemon/data_preprocessing atlants-DATA_preprocessing/preprocess-daemon/config/preprocess_config_player2.xml &
-	atlants-cerebral_wars/cerebral_wars atlants-cerebral_wars/config/cerebral_wars_config.xml &
+	atlants-cerebral_wars/cerebral_wars_app atlants-cerebral_wars/config/cerebral_wars_config.xml &
 else
-    killall cerebral_wars
+    killall cerebral_wars_app
     killall data_interface
     killall data_preprocessing
 fi
